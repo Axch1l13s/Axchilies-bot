d8cd22f7d393774ccb35e15afe07ced0bf9335ec
Axch1l13s <adangmuhamad7@gmail.com>
2026-07-15 01:43:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
+import os
 from flask import Flask
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import threading
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "Axchilies Alpha Bot is Running! 🚀"
+    return "Axchilies Alpha Bot Running!"
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🔥 Welcome to Axchilies Alpha Bot!\n\nBot berhasil online."
+    )
+
+def run_bot():
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
