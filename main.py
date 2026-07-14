9778a5b2b99fcf0f8df742bc814ee69cb871e32c
Axch1l13s <adangmuhamad7@gmail.com>
2026-07-15 01:27:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Axchilies Alpha Bot is Running! 🚀"
+def home():return "Axchilies Alpha Bot is Running! 🚀"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
