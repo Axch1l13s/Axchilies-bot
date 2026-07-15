2e9fd7fd5691867aaa2da84b029750ef3ec17c3d
Axch1l13s <adangmuhamad7@gmail.com>
2026-07-15 14:21:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
-@@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(
         message,
